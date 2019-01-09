567bcf6826d01b43ce692d4ec752b895fc93c666
mhtsbt <matthias.hutsebaut@gmail.com>
2019-01-09 09:50:32 +0100

    added some more exp

--- run_all.sh
+++ run_all.sh
@@ -10,6 +10,11 @@ python agent.py --env PaperMaze-v0 --ao_sampling 0.25 &
 python agent.py --env PaperMaze-v0 --ao_sampling 0.5 &
 python agent.py --env PaperMaze-v0 --ao_sampling 1 &
 
+python agent.py --env PaperMaze-v0 --options 12 &
+python agent.py --env PaperMaze-v0 --ao_sampling 0.25 --options 12 &
+python agent.py --env PaperMaze-v0 --ao_sampling 0.5 --options 12 &
+python agent.py --env PaperMaze-v0 --ao_sampling 1 --options 12 &
+
 python agent.py --env PaperMaze-v0 --iterations 5 &
 python agent.py --env PaperMaze-v0 --ao_sampling 0.25 --iterations 5 &
 python agent.py --env PaperMaze-v0 --ao_sampling 0.5 --iterations 5 &
@@ -19,6 +24,11 @@ python agent.py --env Maze-v0 --iterations 5 &
 python agent.py --env Maze-v0 --ao_sampling 0.25 --iterations 5 &
 python agent.py --env Maze-v0 --ao_sampling 0.5 --iterations 5 &
 python agent.py --env Maze-v0 --ao_sampling 1 --iterations 5 &
+
+python agent.py --env Maze-v0 --iterations 5 --options 16 &
+python agent.py --env Maze-v0 --ao_sampling 0.25 --iterations 5 --options 16 &
+python agent.py --env Maze-v0 --ao_sampling 0.5 --iterations 5 --options 16 &
+python agent.py --env Maze-v0 --ao_sampling 1 --iterations 5 --options 16 &
 
 wait
 echo all experiments completed
